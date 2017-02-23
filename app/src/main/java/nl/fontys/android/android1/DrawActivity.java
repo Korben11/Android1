fa7c1d6bef1bfa907df77617a9ef823cac264e84
Stefan Tod <s.todorov@student.fontys.nl>
2017-02-23 16:20:36 +0100

    Draw activity beggining

--- app/src/main/java/nl/fontys/android/android1/DrawActivity.java
+++ app/src/main/java/nl/fontys/android/android1/DrawActivity.java
@@ -3,12 +3,56 @@ package nl.fontys.android.android1;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Button;
 
 /**
  * Created by Korben on 16.2.2017.
  */
 
 public class DrawActivity extends BaseActivity {
+
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    public DrawActivity(Context context, AttributeSet attrs){
+
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(6f);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.MITER);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                return false;
+        }
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                path.reset();
+            }
+        });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
